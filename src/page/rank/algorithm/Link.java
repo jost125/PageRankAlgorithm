36031f78fff890e9a193fb3bf292cb11b17f1e4f
Jan Machala <machala@collabim.com>
2012-04-08 16:28:25 +0200

    Implemented comparable interface to Link class

--- src/page/rank/algorithm/Link.java
+++ src/page/rank/algorithm/Link.java
@@ -1,6 +1,6 @@
 package page.rank.algorithm;
 
-public class Link {
+public class Link implements Comparable<Link> {
 	private Integer target;
 	private Integer numberOfLinks;
 
@@ -17,5 +17,35 @@ public class Link {
 		return numberOfLinks;
 	}
 
-	
+	@Override
+	public int compareTo(Link link) {
+		return new Integer(this.hashCode()).compareTo(link.hashCode());
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 1;
+		hash = hash * 17 + target;
+		hash = hash * 31 + numberOfLinks;
+
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Link other = (Link) obj;
+		if (this.target != other.target && (this.target == null || !this.target.equals(other.target))) {
+			return false;
+		}
+		if (this.numberOfLinks != other.numberOfLinks && (this.numberOfLinks == null || !this.numberOfLinks.equals(other.numberOfLinks))) {
+			return false;
+		}
+		return true;
+	}
 }
